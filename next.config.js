f658814bae8ffbb95a8385819b3acff0d99d7bf0
Vadim Ilikov <viking2152@gmail.com>
2022-02-21 14:26:52 +0300

    fix fix fix

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ module.exports = {
 		config.module.rules.push({
 			test: /\.(graphql|gql)$/,
 			exclude: /node_modules/,
-			use: [
-				{
-					loader: "graphql-tag/loader",
-				},
-			],
+			loader: "graphql-tag/loader",
 		});
 
 		return config;
--- src/pages/states/index.jsx
+++ src/pages/states/index.jsx
@@ -3,62 +3,19 @@ import GLOBALS from "../../../queries/globals.graphql";
 import GET_ICONS from "../../../queries/getIcons.graphql";
 import Image from "next/image";
 import styled from "styled-components";
+import {useRouter} from "next/router";
 
 
-const Tasks = ({ resultObject, arrStates, codeLocale}) => {
-
-    const Wrapper = styled.div`
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      justify-items: center;
-    `
-
-    return (
-        <div>
-            <div style={{display: "flex"}}>
-                <p>Current language:</p>
-                <span>
-                <Image src={resultObject?.data}
-                       alt={codeLocale}
-                       width="30"
-                       height="30"
-                />
-            </span>
-            </div>
-            <Wrapper>
-                {arrStates.map((element, index) => {
-                    return(
-                        <div key={index}>
-                            <h2>{element}</h2>
-                        </div>
-                    )})}
-            </Wrapper>
-        </div>
-    )
-}
-
-export const getServerSideProps = async (context) => {
-    const apolloClient = initializeApollo();
-
-    const {locale} = context
+const Tasks = ({ icons, states}) => {
 
 
-    const responseStates = await apolloClient.query({
-        query: GLOBALS
-    })
-
-    const responseIcons = await apolloClient.query({
-        query: GET_ICONS
-    })
-
-    const icons = responseIcons.data.icons
-
-    const states = responseStates?.data.settings[0].localization.states
+    const router = useRouter();
+    const t = router.locale || 'ru'
 
     const arrStates = []
 
     for (let key in states) {
-        arrStates.push(states[key][locale].auto)
+        arrStates.push(states[key][t].auto)
     }
     const modificateLocal = locale => {
         switch (locale) {
@@ -107,16 +64,63 @@ export const getServerSideProps = async (context) => {
         }
     };
 
-    let codeLocale = modificateLocal(locale);
+    let codeLocale = modificateLocal(t);
     const resultObject = icons?.find(item => item.code === codeLocale);
+
+    const Wrapper = styled.div`
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      justify-items: center;
+    `
+
+    return (
+        <div>
+            <div style={{display: "flex"}}>
+                <p>Current language:</p>
+                <span>
+                <Image src={resultObject?.data}
+                       alt={codeLocale}
+                       width="30"
+                       height="30"
+                />
+            </span>
+            </div>
+            <Wrapper>
+                {arrStates.map((element, index) => {
+                    return(
+                        <div key={index}>
+                            <h2>{element}</h2>
+                        </div>
+                    )})}
+            </Wrapper>
+        </div>
+    )
+}
+
+
+export const getServerSideProps = async (context) => {
+    const apolloClient = initializeApollo();
+
+    const {locale} = context
+
+    const responseStates = await apolloClient.query({
+        query: GLOBALS
+    })
+
+    const responseIcons = await apolloClient.query({
+        query: GET_ICONS
+    })
+
+    const icons = responseIcons.data.icons
+
+    const states = responseStates?.data.settings[0].localization.states
+
+
 
     return addApolloState(apolloClient, {
         props: {
             states,
             icons,
-            resultObject,
-            codeLocale,
-            arrStates
         }
     })
 }
